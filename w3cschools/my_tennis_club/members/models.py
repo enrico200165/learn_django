1dc16fcbf0f44907e8961b38ee6076c62b71e990
enrico200165 <enrico200165@gmail.com>
2025-07-14 10:00:59 +0200

    saing

--- w3cschools/my_tennis_club/members/models.py
+++ w3cschools/my_tennis_club/members/models.py
@@ -6,3 +6,5 @@ class Member(models.Model):
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    phone = models.IntegerField(null = True)
+    joined_date = models.DateField(null = True)
